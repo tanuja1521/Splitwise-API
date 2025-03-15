13eebe54541d54c8954fbc5ecbeffafe2ae0a3a7
tanuja1521 <tanujareddyarumulla@gmail.com>
2025-03-16 00:16:14 +0530

    Undo Direct commit

--- ExpenseFactory.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-import java.util.Map;
-
-public interface ExpenseFactory {
-    Expense createExpense(String name, double totalAmount, User giver, List<User> users, Map<User, Double> userSplits, SplitStrategy splitStrategy);
-    Expense createExpense(String name, double totalAmount, User giver, List<User> users);
-}
--- SplitStrategy.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-import java.util.Map;
-
-public interface SplitStrategy {
-
-    public Map<User, Double> splitExpense(double totalAmount, List<User> users, Map<User, Double> userSplits);
-}
